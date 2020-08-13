10e6d71db575cfe1da533e1b0d6bbb6728f6ecd6
KleeSchulz <schulz.sebastian98@gmx.de>
2020-08-14 01:33:32 +0200

    Anzeigen der Aufgaben

---
+++ app/src/main/java/com/example/saufio/ac_aufgaben.java
@@ -0,0 +1,13 @@
+package com.example.saufio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ac_aufgaben extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ac_aufgaben);
+    }
+}
